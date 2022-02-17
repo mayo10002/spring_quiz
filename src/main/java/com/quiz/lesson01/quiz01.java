9d371fede5cd03e9b0851412f6688911ee6c0d38
mayo10002 <mayo10002@naver.com>
2022-02-17 20:12:56 +0900

    초기 환경 세팅 quiz

---
+++ src/main/java/com/quiz/lesson01/quiz01.java
@@ -0,0 +1,37 @@
+package com.quiz.lesson01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/quiz/lesson01/quiz01")
+@Controller
+public class quiz01 {
+	
+	@RequestMapping("/0")
+	public String helloworld() {
+		return "HelloWorld!";
+	}
+	
+	// http://localhost:8080/lesson01/quiz01/1
+	@ResponseBody
+	@RequestMapping("/1")
+	public String TestProject() {
+		return "<h2>테스트 프로젝트 완성</h2>해당 프로젝트를 통해서 문제 풀이를 진행합니다.";
+		
+	}
+	@ResponseBody
+	@RequestMapping("/2")
+	public Map<String, Object> quiz1_3() {
+		
+		Map <String, Object> map = new HashMap<>();
+		map.put("국어", 80);
+		map.put("영어", 90);
+		map.put("수학", 85);
+		
+		return map;
+	}
+}
---
+++ src/test/java/com/quiz/test/SpringQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
