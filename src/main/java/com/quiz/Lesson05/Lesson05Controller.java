2537e22fca52d44fa4b9907f09f192cf5bbba0f3
mayo10002 <mayo10002@naver.com>
2022-03-02 21:45:39 +0900

    fmt 라이브러리 끝

--- src/main/java/com/quiz/Lesson05/Lesson05Controller.java
+++ src/main/java/com/quiz/Lesson05/Lesson05Controller.java
@@ -1,6 +1,7 @@
 package com.quiz.Lesson05;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -68,5 +69,50 @@ public class Lesson05Controller {
 		
 		model.addAttribute("membership", membership);
 		return "lesson05/quiz02";
+	}
+	
+	@RequestMapping("lesson05/quiz03")
+	public String quiz03(Model model) {
+		List<Integer> candidates = new ArrayList<>();
+		candidates.add(263001);
+		candidates.add(173942); 
+		candidates.add(563057);
+		
+		model.addAttribute("candidates", candidates);
+		
+		//총 투표 인원 구하기 
+		int totalCount=0;
+		for(int i = 0; i < candidates.size();i++) {
+			totalCount += candidates.get(i);
+		}
+		
+		model.addAttribute("totalCount", totalCount);
+		
+		List<Map<String, Object>> cardBills = new ArrayList<>();
+
+		Map<String, Object> cardBill = new HashMap<>();
+		cardBill.put("store", "GS48");
+		cardBill.put("pay", 7800);
+		cardBill.put("date", "2025-09-14");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "현태백화점");
+		cardBill.put("pay", 2750000);
+		cardBill.put("date", "2025-09-18");
+		cardBill.put("installment", "3개월");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "요촌치킨");
+		cardBill.put("pay", 180000);
+		cardBill.put("date", "2025-09-20");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+		
+		model.addAttribute("cardBills", cardBills);
+		
+		return "lesson05/quiz03";
 	}
 }
